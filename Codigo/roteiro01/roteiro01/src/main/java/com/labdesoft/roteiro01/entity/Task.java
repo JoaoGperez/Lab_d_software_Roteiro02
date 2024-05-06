5bf2168c8090aa71e84e17b8005b8f634e9b23fb
João Gabriel Perez <104282903+JoaoGperez@users.noreply.github.com>
2024-05-06 08:10:28 -0300

    att entity

--- Codigo/roteiro01/roteiro01/src/main/java/com/labdesoft/roteiro01/entity/Task.java
+++ Codigo/roteiro01/roteiro01/src/main/java/com/labdesoft/roteiro01/entity/Task.java
@@ -1,6 +1,6 @@
 package com.labdesoft.roteiro01.entity;
 
-import io.swagger.v3.oas.annotations.media.Schema;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +10,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Size;
+
 
 @Entity
 @Getter
