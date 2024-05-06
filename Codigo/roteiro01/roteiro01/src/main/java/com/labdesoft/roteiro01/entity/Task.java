656d3755a7dc1a213b5038f57fcd497de7253072
João Gabriel Perez <104282903+JoaoGperez@users.noreply.github.com>
2024-05-06 08:08:46 -0300

    Att Entity
    Resolveu o problema do entity, persistence javax para jakarta

--- Codigo/roteiro01/roteiro01/src/main/java/com/labdesoft/roteiro01/entity/Task.java
+++ Codigo/roteiro01/roteiro01/src/main/java/com/labdesoft/roteiro01/entity/Task.java
@@ -1,57 +1,59 @@
 package com.labdesoft.roteiro01.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
 
-//@Entity
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Task {
-  //  @Id
- //   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
     private Boolean completed;
 
-  // Construtor
-  public Task(long id, String description, boolean completed) {
-    this.id = id;
-    this.description = description;
-    this.completed = completed;
-  }
+    // Construtor
+    public Task(long id, String description, boolean completed) {
+        this.id = id;
+        this.description = description;
+        this.completed = completed;
+    }
 
-  // Getters e Setters
-  public long getId() {
-    return id;
-  }
+    // Getters e Setters
+    public long getId() {
+        return id;
+    }
 
-  public void setId(long id) {
-    this.id = id;
-  }
+    public void setId(long id) {
+        this.id = id;
+    }
 
-  public String getDescription() {
-    return description;
-  }
+    public String getDescription() {
+        return description;
+    }
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-  public boolean isCompleted() {
-    return completed;
-  }
+    public boolean isCompleted() {
+        return completed;
+    }
 
-  public void setCompleted(boolean completed) {
-    this.completed = completed;
-  }
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 
 }
