9c49427daaf33440d4e81117f891b1b651436b0d
João Gabriel Perez <104282903+JoaoGperez@users.noreply.github.com>
2024-05-06 17:26:13 -0300

    att service

--- Codigo/roteiro01/roteiro01/src/main/java/com/labdesoft/roteiro01/service/TaskService.java
+++ Codigo/roteiro01/roteiro01/src/main/java/com/labdesoft/roteiro01/service/TaskService.java
@@ -1,4 +1,32 @@
 package com.labdesoft.roteiro01.service;
 
+import com.labdesoft.roteiro01.dto.TaskDto;
+import com.labdesoft.roteiro01.entity.Task;
+import com.labdesoft.roteiro01.repository.TaskRepository;
+//import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Page<Task> listAll(Pageable pageable) {
+        return taskRepository.findAll(pageable);
+    }
+
+    public Task createTask(TaskDto taskDto) {
+        Task task = new Task();
+        taskDto.setId(taskDto.getId());
+        taskDto.setDesctiption(taskDto.getDesctiption());
+        taskDto.setCompleted(false);
+        taskDto.setType(taskDto.getType());
+        taskDto.setPriority(taskDto.getPriority());
+        return taskRepository.save(task);
+    }
 }
