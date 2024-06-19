7a101a28b5cc1b65d0b3fb3310e3dbb023146f02
João Gabriel Perez <104282903+JoaoGperez@users.noreply.github.com>
2024-06-19 10:57:16 -0300

    att front

--- Codigo/roteiro01/front/src/components/TodoWrapper.jsx
+++ Codigo/roteiro01/front/src/components/TodoWrapper.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TodoForm } from './TodoForm';
 import { TodoList } from './TodoList';
+import { v4 as uuidv4 } from "uuid";
 import './TodoWrapper.css';
+
 export const TodoWrapper = () => {
+
+    const [todos, setTodos] = useState([
+        { id: 1, description: 'Tarefa exemplo', completed: false }
+    ]);
+    
+
+
     return (
         <div className='TodoWrapper'>
             <h1>Lista de Tarefas</h1>
             <TodoForm />
-            <TodoList />
+            {todos.map((item) =>
+                <TodoList
+                    key={item.id}
+                    task={item}
+                />
+            )
+            }
         </div>
-        
     );
 }
